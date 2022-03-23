555df772628377820c39e106a7ea675a00514466
Abbos Kamalov <a.kamalov@profsoft.pro>
2022-03-23 17:49:57 +0400

    Fix application controller

--- b2call/src/Controller/Api/ApplicationApiController.php
+++ b2call/src/Controller/Api/ApplicationApiController.php
@@ -7,6 +7,7 @@ use App\Entity\Application;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Nelmio\ApiDocBundle\Annotation\Model;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use OpenApi\Annotations as OA;
 use App\Helper\Exception\ApiException;
@@ -26,7 +27,7 @@ class ApplicationApiController extends AbstractApiController
      *     description="Заявка",
      *     @OA\JsonContent(
      *          @OA\Property(property="data", type="object",
-     *              ref=@Model(type=Application::class)
+     *              ref=@Model(type=Application::class, groups={"show_app"})
      *          )
      *     )
      * )
@@ -42,9 +43,16 @@ class ApplicationApiController extends AbstractApiController
         return $this->json(['data' => $application], context: ["groups" => ["show_app"]]);
     }
 
-    #[Route('', name: 'create_application', methods: ['POST'])]
+    #[Route('', name: 'create_full_application', methods: ['POST'])]
     /**
-     * Создание заявки c общей информацией
+     * Создание или редактирование заявки
+     *
+     * @OA\Parameter (
+     *     name="id",
+     *     in="query",
+     *     required=false,
+     *     description="id редактируемой заявки"
+     * )
      *
      * @OA\RequestBody  (
      *     required=true,
@@ -53,13 +61,25 @@ class ApplicationApiController extends AbstractApiController
      *          @OA\Property(property="source", type="string", example="Источник"),
      *          @OA\Property(property="space", type="string", example="Необходимое пространство"),
      *          @OA\Property(property="room", type="string", example="Назначение помещения"),
-     *          @OA\Property(property="typeRoom", type="string", example="Тип помещения")
+     *          @OA\Property(property="typeRoom", type="string", example="Тип помещения"),
+     *          @OA\Property(property="floorFrom", type="integer", description="Этаж от"),
+     *          @OA\Property(property="floorTo", type="integer", description="Этаж до"),
+     *          @OA\Property(property="builtFrom", type="integer", description="Построен от"),
+     *          @OA\Property(property="builtTo", type="integer", description="Построен до"),
+     *          @OA\Property(property="typeBuilding", type="string", example="тип здания"),
+     *          @OA\Property(property="entrance", type="string", example="вход"),
+     *          @OA\Property(property="furnitureAvailable", type="boolean", description="наличие мебели"),
+     *          @OA\Property(property="conditioner", type="boolean", description="кондиционер"),
+     *          @OA\Property(property="heating", type="boolean", description="отопление"),
+     *          @OA\Property(property="internet", type="boolean", description="интернет"),
+     *          @OA\Property(property="kitchen", type="boolean", description="кухня"),
+     *          @OA\Property(property="entrancePrivate", type="boolean", description="отдельный вход")
      *     )
      * )
      *
      * @OA\Response(
      *     response="200",
-     *     description="Создано",
+     *     description="Создана заявка",
      *     @OA\JsonContent(
      *          @OA\Property(property="data", type="object",
      *              @OA\Property(property="id", type="integer")
@@ -70,50 +90,36 @@ class ApplicationApiController extends AbstractApiController
      */
     public function createApplication(Request $request): JsonResponse
     {
-        $application = $this->serializer->deserialize($request->getContent(), Application::class, "json");
-        $this->entityManager->getRepository(Application::class)->add($application, true);
+        $id = $request->get("id", null);
+        $application = $this->entityManager->getRepository(Application::class)->findOneBy(['id' => $id]);
+        if(!$application && $id)
+            throw new ApiException('Not found application', status: Response::HTTP_NOT_FOUND);
+        elseif ($application){
+            $data = $this->serializer->deserialize($request->getContent(), Application::class, "json");
+            $application->setService($data->getService() ?? $application->getService());
+            $application->setEntrance($data->getEntrance() ?? $application->getEntrance());
+            $application->setConditioner($data->getConditioner() ?? $application->getConditioner());
+            $application->setTypeRoom($data->getTypeRoom() ?? $application->getTypeRoom());
+            $application->setBuiltTo($data->getBuiltTo() ?? $application->getBuiltTo());
+            $application->setBuiltFrom($data->getBuiltFrom() ?? $application->getBuiltFrom());
+            $application->setSource($data->getSource() ?? $application->getSource());
+            $application->setSpace($data->getSpace() ?? $application->getSpace());
+            $application->setRoom($data->getRoom() ?? $application->getRoom());
+            $application->setFloorFrom($data->getFloorFrom() ?? $application->getFloorFrom());
+            $application->setFloorTo($data->getFloorTo() ?? $application->getFloorTo());
+            $application->setTypeBuilding($data->getTypeBuilding() ?? $application->getTypeBuilding());
+            $application->setEntrance($data->getEntrance() ?? $application->getEntrance());
+            $application->setFurnitureAvailable($data->getFurnitureAvailable() ?? $application->getFurnitureAvailable());
+            $application->setHeating($data->getHeating() ?? $application->getHeating());
+            $application->setInternet($data->getInternet() ?? $application->getInternet());
+            $application->setKitchen($data->getKitchen() ?? $application->getKitchen());
+            $application->setEntrancePrivate($data->getEntrancePrivate() ?? $application->getEntrancePrivate());
+        }else{
+            $application = $this->serializer->deserialize($request->getContent(), Application::class, "json");
+            $this->entityManager->persist($application);
+        }
+        $this->entityManager->flush();
         return $this->json(['data' => $application->getId()]);
     }
 
-    #[Route('', name: 'create_full_application', methods: ['POST'])]
-    /**
-     * Создание заявки c полной информацией
-     *
-     * @OA\RequestBody  (
-     *     required=true,
-     *     @OA\JsonContent(
-     *          @OA\Property(property="service", type="string", example="Услуга"),
-     *          @OA\Property(property="source", type="string", example="Источник"),
-     *          @OA\Property(property="space", type="string", example="Необходимое пространство"),
-     *          @OA\Property(property="room", type="string", example="Назначение помещения"),
-     *          @OA\Property(property="typeRoom", type="string", example="Тип помещения"),
-     *          @OA\Property(property="floorFrom", type="integer", example="Этаж от),
-     *          @OA\Property(property="floorTo", type="integer", example="Этаж до"),
-     *          @OA\Property(property="builtFrom", type="integer", example="Построен от"),
-     *          @OA\Property(property="builtTo", type="integer", example="Построен до"),
-     *          @OA\Property(property="typeBuilding", type="string", example="тип здания"),
-     *          @OA\Property(property="entrance", type="string", example="вход"),
-     *          @OA\Property(property="furnitureAvailable", type="boolean", example="false, наличие мебели"),
-     *          @OA\Property(property="conditioner", type="boolean", example="false, кондиционер"),
-     *          @OA\Property(property="heating", type="boolean", example="false, отопление")
-     *     )
-     * )
-     *
-     * @OA\Response(
-     *     response="200",
-     *     description="Создано",
-     *     @OA\JsonContent(
-     *          @OA\Property(property="data", type="object",
-     *              @OA\Property(property="id", type="integer")
-     *          )
-     *     )
-     * )
-     *
-     */
-    public function createFullApplication(Request $request): JsonResponse
-    {
-        $application = $this->serializer->deserialize($request->getContent(), Application::class, "json");
-        $this->entityManager->getRepository(Application::class)->add($application, true);
-        return $this->json(['data' => $application->getId()]);
-    }
 }
--- b2call/src/Entity/Application.php
+++ b2call/src/Entity/Application.php
@@ -39,6 +39,54 @@ class Application
     #[Groups(["show_app"])]
     private $client;
 
+    #[ORM\Column(type: 'integer', nullable: true)]
+    #[Groups(["show", "show_app"])]
+    private $floorFrom;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    #[Groups(["show", "show_app"])]
+    private $floorTo;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    #[Groups(["show", "show_app"])]
+    private $builtFrom;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    #[Groups(["show", "show_app"])]
+    private $builtTo;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    #[Groups(["show", "show_app"])]
+    private $typeBuilding;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    #[Groups(["show", "show_app"])]
+    private $entrance;
+
+    #[ORM\Column(type: 'boolean', nullable: true)]
+    #[Groups(["show", "show_app"])]
+    private $furnitureAvailable;
+
+    #[ORM\Column(type: 'boolean', nullable: true)]
+    #[Groups(["show", "show_app"])]
+    private $conditioner;
+
+    #[ORM\Column(type: 'boolean', nullable: true)]
+    #[Groups(["show", "show_app"])]
+    private $heating;
+
+    #[ORM\Column(type: 'boolean', nullable: true)]
+    #[Groups(["show", "show_app"])]
+    private $internet;
+
+    #[ORM\Column(type: 'boolean', nullable: true)]
+    #[Groups(["show", "show_app"])]
+    private $kitchen;
+
+    #[ORM\Column(type: 'boolean', nullable: true)]
+    #[Groups(["show", "show_app"])]
+    private $entrancePrivate;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -115,4 +163,168 @@ class Application
 
         return $this;
     }
+
+    public function getFloorFrom(): ?int
+    {
+        return $this->floorFrom;
+    }
+
+    public function setFloorFrom(?int $floorFrom): self
+    {
+        $this->floorFrom = $floorFrom;
+
+        return $this;
+    }
+
+    public function getFloorTo(): ?int
+    {
+        return $this->floorTo;
+    }
+
+    public function setFloorTo(?int $floorTo): self
+    {
+        $this->floorTo = $floorTo;
+
+        return $this;
+    }
+
+    public function getBuiltFrom(): ?int
+    {
+        return $this->builtFrom;
+    }
+
+    public function setBuiltFrom(?int $builtFrom): self
+    {
+        $this->builtFrom = $builtFrom;
+
+        return $this;
+    }
+
+    public function getBuiltTo(): ?int
+    {
+        return $this->builtTo;
+    }
+
+    public function setBuiltTo(?int $builtTo): self
+    {
+        $this->builtTo = $builtTo;
+
+        return $this;
+    }
+
+    public function getTypeBuilding(): ?string
+    {
+        return $this->typeBuilding;
+    }
+
+    public function setTypeBuilding(?string $typeBuilding): self
+    {
+        $this->typeBuilding = $typeBuilding;
+
+        return $this;
+    }
+
+    public function getEntrance(): ?string
+    {
+        return $this->entrance;
+    }
+
+    public function setEntrance(?string $entrance): self
+    {
+        $this->entrance = $entrance;
+
+        return $this;
+    }
+
+    public function getFurnitureAvailable(): ?bool
+    {
+        return $this->furnitureAvailable;
+    }
+
+    public function setFurnitureAvailable(?bool $furnitureAvailable): self
+    {
+        $this->furnitureAvailable = $furnitureAvailable;
+
+        return $this;
+    }
+
+    public function getConditioner(): ?bool
+    {
+        return $this->conditioner;
+    }
+
+    public function setConditioner(?bool $conditioner): self
+    {
+        $this->conditioner = $conditioner;
+
+        return $this;
+    }
+
+    public function getHeating(): ?bool
+    {
+        return $this->heating;
+    }
+
+    public function setHeating(?bool $heating): self
+    {
+        $this->heating = $heating;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getInternet()
+    {
+        return $this->internet;
+    }
+
+    /**
+     * @param mixed $internet
+     * @return Application
+     */
+    public function setInternet($internet)
+    {
+        $this->internet = $internet;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getKitchen()
+    {
+        return $this->kitchen;
+    }
+
+    /**
+     * @param mixed $kitchen
+     * @return Application
+     */
+    public function setKitchen($kitchen)
+    {
+        $this->kitchen = $kitchen;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEntrancePrivate()
+    {
+        return $this->entrancePrivate;
+    }
+
+    /**
+     * @param mixed $entrancePrivate
+     * @return Application
+     */
+    public function setEntrancePrivate($entrancePrivate)
+    {
+        $this->entrancePrivate = $entrancePrivate;
+        return $this;
+    }
+
+
 }
